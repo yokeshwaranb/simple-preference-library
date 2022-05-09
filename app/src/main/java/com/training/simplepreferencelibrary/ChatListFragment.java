8f97799582e8452a773ed752c2a3f0875e7d6627
yokesh7373@gmail.com <gitHub@1>
2022-05-09 22:11:47 +0530

    Created the Fragments.

---
+++ app/src/main/java/com/training/simplepreferencelibrary/ChatListFragment.java
@@ -0,0 +1,23 @@
+package com.training.simplepreferencelibrary;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ChatListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.fragment_chat_list, container, false);
+    }
+
+}
