743393022272b47c31f71758c596ec1e99785521
yokesh7373@gmail.com <gitHub@1>
2022-06-26 20:28:44 +0530

    Implemented onClickListener for Preference object.

--- app/src/main/java/com/training/simplepreferencelibrary/SettingsFragment.java
+++ app/src/main/java/com/training/simplepreferencelibrary/SettingsFragment.java
@@ -2,7 +2,9 @@ package com.training.simplepreferencelibrary;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
@@ -10,5 +12,17 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         setPreferencesFromResource(R.xml.settings, rootKey);
+
+        Preference accountSettingsPref = findPreference(getResources().getString(R.string.key_account_settings));
+
+        accountSettingsPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(@NonNull Preference preference) {
+
+                // Do Something
+
+                return true;
+            }
+        });
     }
 }
