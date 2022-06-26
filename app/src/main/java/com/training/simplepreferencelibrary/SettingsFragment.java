40cc9ca9969f028927e4986e7d0b588e4e465f02
yokesh7373@gmail.com <gitHub@1>
2022-06-26 18:41:19 +0530

    Simple SwitchPreference Implementation.

--- app/src/main/java/com/training/simplepreferencelibrary/SettingsFragment.java
+++ app/src/main/java/com/training/simplepreferencelibrary/SettingsFragment.java
@@ -1,36 +1,14 @@
 package com.training.simplepreferencelibrary;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.NavDirections;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.preference.PreferenceFragmentCompat;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_settings, container, false);
-
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Navigate to AccountSettingsFragment
-                NavHostFragment navHostFragment = (NavHostFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-                NavController navController = navHostFragment.getNavController();
-                NavDirections direction = SettingsFragmentDirections.actionSettingsFragmentToAccountSettingsFragment();
-                navController.navigate(direction);
-            }
-        });
-
-        return view;
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+        setPreferencesFromResource(R.xml.settings, rootKey);
     }
-
 }
